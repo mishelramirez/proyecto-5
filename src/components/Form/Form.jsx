dc0f825767ee90741979054071fd8965ec0b0794
cintya <rmishel564@gmail.com>
2023-04-17 16:37:51 -0500

    se da validacion al formulario con hook y se dan estilos en bomile al 100%

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleExclamation, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons"
 import style from "./form.css"
+import { useForm } from "react-hook-form";
 
-function Form() {
+const Form = () => {
+
+    const{ register, formState:{errors}, handleSubmit } = useForm();
+
+    const onSubmit = (data)=>{
+        console.log(data)
+    }
+ 
     return <div className="Rightside">
 
         <div className="formheader">
@@ -10,39 +18,39 @@ function Form() {
         </div>
 
         <div className="formcontainer">
-            <form action="" className="formulario" id="formulario">
+            <form onSubmit={handleSubmit(onSubmit)} className="formulario" id="formulario">
 
                 <div className="forgrup " id="grupname">
                     <div className="formgrupimput forgrupcorrecto">
-                        <input type="text" className="formimput" name="nombre" id="nombre" placeholder="Nombre" />
+                        <input  type="text" className="formimput" {...register("name", {required:true})}  placeholder="Nombre" />
                         <FontAwesomeIcon className="formvalidacion" icon={faCircleExclamation} style={{ color: "#ce70e1", }} />
                     </div>
-                    <p className="texterror">First Name cannot be ompty</p>
+                    {errors.name?.type ==="required" && <p className="texterror">First Name cannot be ompty</p>}
                 </div>
 
 
                 <div className="forgrup" id="grupname">
                     <div className="formgrupimput forgrupincorrecto">
-                        <input type="text" className="formimput" name="Lastnames" id="Lastname" placeholder="Last Name" />
+                        <input type="text" className="formimput"  {...register("lastname", {required:true})} placeholder="Last Name" />
                         <FontAwesomeIcon className="formvalidacion" icon={faCircleExclamation} style={{ color: "#ce70e1", }} />
                     </div>
-                    <p className="texterror">Last Name cannot be ompty</p>
+                    {errors.lastname?.type ==="required" && <p className="texterror">Last Name cannot be ompty</p>}
                 </div>
 
                 <div className="forgrup" id="grupemail">
                     <div className="formgrupimput">
-                        <input type="email" className="formimput" name="email" id="email" placeholder="email@email.com" />
+                        <input type="email" className="formimput"  placeholder="email@email.com" {...register("email", {pattern: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/})} />
                         <FontAwesomeIcon className="formvalidacion" icon={faCircleExclamation} style={{ color: "#ce70e1", }} />
                     </div>
-                    <p className="texterror">Look like this is not an email</p>
+                    {errors.email?.type ==="pattern" &&<p className="texterror">Look like this is not an email</p>}
                 </div>
 
                 <div className="forgrup" id="gruppassword">
                     <div className="formgrupimput">
-                        <input type="password" className="formimput" name="password" id="password" placeholder="password" />
+                        <input type="password" className="formimput" {...register("password", {required:true})} placeholder="password" />
                         <FontAwesomeIcon className="formvalidacion" icon={faCircleExclamation} style={{ color: "#ce70e1", }} />
                     </div>
-                    <p className="texterror">Password cannot be ompty</p>
+                    {errors.password?.type ==="required" && <p className="texterror">Password cannot be ompty</p>}
                 </div>
 
                 <div className="message " id="message">
@@ -51,7 +59,7 @@ function Form() {
                 </div>
 
                 <div className="grupterminos grup__form__btn" id="terminos">
-                    <button type="submit" className="btn">CLAIM YOUR FFREE TRIAL</button>
+                    <input type="submit" className="btn" value="CLAIM YOUR FFREE TRIAL" />
                     <p className="messegetext" id="messegetext">The form was sent successfully</p>
                 </div>
 
